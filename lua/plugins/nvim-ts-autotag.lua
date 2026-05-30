8c172b82c21dbb893e15ac0f7e29976a2910e76f
Zach Bugay <Zachary.Bugay@outlook.com>
2026-05-30 06:56:44 -0400

    chore(autopairs): Auto pair and auto close html tags

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,10 @@
+return {
+  "windwp/nvim-ts-autotag",
+  event = { "BufReadPre", "BufNewFile" },
+  -- opts = {
+  --   enable_close = true,
+  --   enable_rename = true,
+  -- },
+  -- use opts = {} for passing setup options
+  -- this is equivalent to setup({}) function
+}
