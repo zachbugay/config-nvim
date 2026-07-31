f532c361f15656b5f5b0eb4a549e743377f5b238
Zach Bugay <zacharybugay@microsoft.com>
2026-07-31 07:11:12 -0400

    fix(autocmds): Highlight yanked

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.hl_op()
   end,
 })
 
