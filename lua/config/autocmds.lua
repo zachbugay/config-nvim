ef10d33ea4437941957caa42bb3a70fb2a9a80f5
Zach Bugay <zacharybugay@microsoft.com>
2026-09-03 09:17:24 -0400

    feat(autocmds): Fixed TextYankPost.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
-    vim.hl.hl_op()
+    vim.hl.on_yank()
   end,
 })
 
